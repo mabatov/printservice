5d0c706b17a3fc5b7f3a839079d2c7bdcdee922e
mabatov <nikita-mabatov@mail.ru>
2020-11-05 10:53:38 +0300

    added footer icons, try to create hrefs

--- src/Footer.js
+++ src/Footer.js
@@ -3,9 +3,14 @@ import React from "react";
 const Footer = () => (
     <div className="footer">
         Связаться:
-            <a href="https://vk.com/print_romantic" target="_blank" title="VK"> VK </a>
-            <a href="https://t.me/print_romantic" target="_blank" title="Telegram">TG</a>
-            <a href="mailto:print_romantic@mail.ru" title="Написать письмо"> ✉️</a>
+        <a href="https://vk.com/print_romantic" target="_blank" rel="noreferrer" title="VK"> VK </a>
+        <a href="https://t.me/print_romantic" target="_blank" rel="noreferrer" title="Telegram">TG</a>
+        <a href="mailto:print_romantic@mail.ru" title="Написать письмо"> ✉️</a>
+
+        <a href="https://vk.com/print_romantic"><img src="./img/social/vk32.png" alt='Написать ВК' title='Написать ВК'/></a>
+        <a href="https://t.me/print_romantic"><img src="./img/social/tg32.png" alt='Написать в телеграм' title='Написать в телеграм'/></a>
+        <a href="https://vk.com/print_romantic"><img src="./img/social/ml32.png" alt='Написать на почту' title='Написать на почту'/></a>
+
     </div>
 );
 
