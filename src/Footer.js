934a97c274ecea4c60a4d00df8296e246c6ca34c
mabatov <nikita-mabatov@mail.ru>
2020-11-05 21:22:36 +0300

    changed pricedesc content, added correct mailto link

--- src/Footer.js
+++ src/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => (
         <p>
         <a if='img_footer' class='tc ma1' href="https://vk.com/print_romantic" target="_blank" rel="noreferrer"><img src={VK} alt='Написать ВК' title='Написать ВК'/></a>
         <a if='img_footer' class='tc ma1' href="https://t.me/print_romantic" target="_blank" rel="noreferrer"><img src={TG} alt='Написать в телеграм' title='Написать в телеграм'/></a>
-        <a if='img_footer' class='tc ma1' href="https://vk.com/print_romantic" target="_blank" rel="noreferrer"><img src={ML} alt='Написать на почту' title='Написать на почту'/></a>
+        <a if='img_footer' class='tc ma1' href="mailto:print_romantic@mail.ru" target="_blank" rel="noreferrer"><img src={ML} alt='Написать на почту' title='Написать на почту'/></a>
         </p>
 
     </div>
