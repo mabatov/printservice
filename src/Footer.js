d1e34ad636a77fd7355934fa0fb1d5bfe40029ae
mabatov <nikita-mabatov@mail.ru>
2020-11-05 20:42:07 +0300

    img's in footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,22 @@
 import React from "react";
+import VK from './img/social/vk32.png'
+import TG from './img/social/tg32.png'
+import ML from './img/social/ml32.png'
 
 const Footer = () => (
     <div className="footer">
-        Связаться:
+        {/*Связаться:
         <a href="https://vk.com/print_romantic" target="_blank" rel="noreferrer" title="VK"> VK </a>
-        <a href="https://t.me/print_romantic" target="_blank" rel="noreferrer" title="Telegram">TG</a>
-        <a href="mailto:print_romantic@mail.ru" title="Написать письмо"> ✉️</a>{/*
+        <a href="https://t.me/print_romantic" target="_blank" rel="noreferrer" title="Telegram">TG </a>
+        <a href="mailto:print_romantic@mail.ru" title="Написать письмо"> ✉️</a>*/}
 
-        <a href="https://vk.com/print_romantic"><img src="./img/social/vk32.png" alt='Написать ВК' title='Написать ВК'/></a>
-        <a href="https://t.me/print_romantic"><img src="./img/social/tg32.png" alt='Написать в телеграм' title='Написать в телеграм'/></a>
-        <a href="https://vk.com/print_romantic"><img src="./img/social/ml32.png" alt='Написать на почту' title='Написать на почту'/></a>*/}
+
+        Напишите нам:
+        <p>
+        <a if='img_footer' class='tc ma1' href="https://vk.com/print_romantic" target="_blank" rel="noreferrer"><img src={VK} alt='Написать ВК' title='Написать ВК'/></a>
+        <a if='img_footer' class='tc ma1' href="https://t.me/print_romantic" target="_blank" rel="noreferrer"><img src={TG} alt='Написать в телеграм' title='Написать в телеграм'/></a>
+        <a if='img_footer' class='tc ma1' href="https://vk.com/print_romantic" target="_blank" rel="noreferrer"><img src={ML} alt='Написать на почту' title='Написать на почту'/></a>
+        </p>
 
     </div>
 );
